0b4f0eb0f3e2dccccd610f77732e6434cda43750
Jon Lee <jon.lee@globalrelay.net>
2022-02-21 00:01:03 -0800

    Initial Setup

---
+++ src/base.js
@@ -0,0 +1,14 @@
+import firebase from 'firebase';
+import 'firebase/storage';
+
+export const app = firebase.initializeApp({
+  "projectId": "dear-lereve",
+  "appId": "1:663343050719:web:027aa72a2a2ac020555e6a",
+  "storageBucket": "dear-lereve.appspot.com",
+  "locationId": "us-west1",
+  "apiKey": "AIzaSyAQ5lEjDOWCpGPWkLGSv4CbmXuR05AERS4",
+  "authDomain": "dear-lereve.firebaseapp.com",
+  "messagingSenderId": "663343050719",
+  "measurementId": "G-CX3YCF2MM2"
+});
+
