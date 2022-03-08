8d54933da0f166f3b78a666cfc380d21e9e6137b
Jon Lee <jon.lee@globalrelay.net>
2022-03-07 22:41:14 -0800

    Sample MUI page

--- src/App.js
+++ src/App.js
@@ -2,13 +2,15 @@ import './App.css';
 import { Routes, Route } from 'react-router-dom';
 import AdminLogin from './features/admin/AdminLogin.component';
 import ProductRegister from './features/admin/ProductRegister.component';
+import Main from './features/main/Main.component';
 import Products from './features/products/Products.components';
 import { PrivateRoute } from './utils/PrivateRoute';
 
 function App() {
   return (
     <Routes>
-      <Route path="/" element={<Products></Products>} />
+      <Route path="/" element={<Main></Main>} />
+      <Route path="/products" element={<Products></Products>} />
       <Route path="/login" element={<AdminLogin />} />
       <Route
         path="/admin"
