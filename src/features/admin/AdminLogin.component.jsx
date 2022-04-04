f13ab6148447f47d04d05ef35f6797e9316d0a1a
Jon Lee <jon.lee@globalrelay.net>
2022-04-03 22:50:49 -0700

    Login page enhancement

--- src/features/admin/AdminLogin.component.jsx
+++ src/features/admin/AdminLogin.component.jsx
@@ -4,13 +4,22 @@ import { useNavigate } from 'react-router-dom';
 import { setCredentials } from './authSlice';
 import { auth } from '../../base';
 import { signInWithEmailAndPassword } from 'firebase/auth';
+import TextField from '@mui/material/TextField';
+import Box from '@mui/material/Box';
+import Button from '@mui/material/Button';
+import Checkbox from '@mui/material/Checkbox';
+import FormGroup from '@mui/material/FormGroup';
+import FormControlLabel from '@mui/material/FormControlLabel';
 
 const AdminLogin = () => {
   const dispatch = useDispatch();
   const navigate = useNavigate();
-  const [username, setUsername] = useState('');
+  const [username, setUsername] = useState(localStorage.getItem('username'));
   const [pwd, setPwd] = useState('');
   const [errorMsg, setErrorMsg] = useState('');
+  const [saveUsername, setSaveUsername] = useState(
+    !!localStorage.getItem('username')
+  );
 
   const login = (e) => {
     e.preventDefault();
@@ -20,6 +29,11 @@ const AdminLogin = () => {
         // Signed in
         const user = userCredential.user;
         dispatch(setCredentials({ user: user.uid, token: user.accessToken }));
+
+        if (saveUsername) {
+          localStorage.setItem('username', username);
+        }
+
         navigate('/admin');
       })
       .catch((error) => {
@@ -29,32 +43,73 @@ const AdminLogin = () => {
         console.error('Login failed with errorMessage: ', errorMessage);
 
         setErrorMsg(errorMessage);
+        setUsername('');
+        setPwd('');
+        setSaveUsername(false);
       });
   };
 
   return (
     <form onSubmit={login}>
-      <div>{errorMsg}</div>
-      <label htmlFor="username">Username:</label>
-      <input
-        id="username"
-        value={username}
-        onChange={(e) => {
-          setUsername(e.target.value);
+      <Box
+        sx={{
+          padding: 2,
         }}
-      />
-      <br />
-      <label htmlFor="pasword">Password:</label>
-      <input
-        id="password"
-        type="password"
-        value={pwd}
-        onChange={(e) => {
-          setPwd(e.target.value);
-        }}
-      />
-      <br />
-      <button type="submit">Submit</button>
+      >
+        <div>{errorMsg}</div>
+
+        <TextField
+          sx={{
+            marginBottom: 2,
+          }}
+          label="Username"
+          fullWidth
+          variant="outlined"
+          value={username}
+          onChange={(e) => {
+            setUsername(e.target.value);
+          }}
+        />
+
+        <TextField
+          sx={{
+            marginBottom: 2,
+          }}
+          type="password"
+          label="Password"
+          fullWidth
+          variant="outlined"
+          value={pwd}
+          onChange={(e) => {
+            setPwd(e.target.value);
+          }}
+        />
+
+        <FormGroup>
+          <FormControlLabel
+            control={
+              <Checkbox
+                checked={saveUsername}
+                onChange={(event) => {
+                  setSaveUsername(event.target.checked);
+                }}
+              />
+            }
+            label="Save username"
+          />
+        </FormGroup>
+
+        <Button
+          sx={{
+            marginTop: 2,
+          }}
+          variant="contained"
+          type="submit"
+          disabled={!username || !pwd}
+        >
+          Submit
+        </Button>
+      </Box>
     </form>
   );
 };
